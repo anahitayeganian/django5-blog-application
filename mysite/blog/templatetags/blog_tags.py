516a38f6583d4c20458af4cdc737f4d897ce94f9
Anahita Yeganian <anahitayeganian@gmail.com>
2025-07-26 10:59:55 -0400

    Use inclusion tag to display latest posts in the sidebar
    - Added show_latest_posts as an inclusion tag in blog_tags.py, which queries the latest published posts ordered by publication date and renders them using the latest_posts.html template.
    - Updated base.html to call the inclusion tag and display the latest 3 posts in the sidebar.
    - Adjusted styles in blog.css for sidebar headings and list items.

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -8,3 +8,10 @@ register = template.Library()
 def total_posts():
     # Return the count of all posts using the custom published manager
     return Post.published.count()
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    # Get the latest 'count' published posts, ordered by descending publication_date
+    latest_posts = Post.published.order_by('-publication_date')[:count]
+    # Return the posts in a dictionary to render them in the latest_posts.html template
+    return {'latest_posts': latest_posts}
